df64696cff6a8347202df0e18b4fb8cb70aba43d
AdrienDhmx <105926516+AdrienDhmx@users.noreply.github.com>
2024-02-16 15:38:40 +0100

    init: flame and assets

---
+++ lib/brocode.dart
@@ -0,0 +1,13 @@
+
+
+import 'dart:ui';
+
+import 'package:flame/game.dart';
+
+class Brocode extends FlameGame {
+
+  @override
+  Color backgroundColor() {
+    return const Color.fromARGB(255, 173, 223, 247);
+  }
+}
