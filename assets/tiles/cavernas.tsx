a59b6648cda579f2aa01eee4a8632c84abd90284
AdrienDhmx <105926516+AdrienDhmx@users.noreply.github.com>
2024-02-22 10:35:04 +0100

    feat: rendering the map

--- assets/tiles/cavernas.tsx
+++ assets/tiles/cavernas.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="cavernas" tilewidth="8" tileheight="8" tilecount="96" columns="8">
- <image source="../../../Users/adrie/Downloads/cavesofgallet/cavesofgallet_tiles.png" width="64" height="96"/>
+ <image source="map_sprites/cavesofgallet_tiles.png" width="64" height="96"/>
 </tileset>
