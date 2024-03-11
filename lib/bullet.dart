5925cb55a0cda8306e60302659a970b45e66d6a1
Paul Menard <paulmenard07104@gmail.com>
2024-03-11 15:16:36 +0100

    add: player's shot orientation

--- lib/bullet.dart
+++ lib/bullet.dart
@@ -1,22 +1,31 @@
 import 'dart:async';
 
 import 'package:flame/components.dart';
-
 import 'brocode.dart';
 
 class Bullet extends SpriteComponent with HasGameRef<Brocode>{
   Bullet({required Vector2 position}) : super(position: position);
+
   double moveSpeed = 600;
+  late Vector2 direction;
+
   @override
   FutureOr<void> onLoad(){
     sprite = Sprite(game.images.fromCache('bullet_sprites/Bullet.png'));
     scale = Vector2.all(3);
+    anchor = Anchor.center;
+
+    direction = game.cursorPosition - (position - game.camera.viewfinder.position + game.size/2);
+    direction.y = -direction.y;
+    angle = direction.angleToSigned(Vector2(1, 0));
+    direction.y = -direction.y;
+
     return super.onLoad();
   }
 
   @override
   void update(double dt) {
-    position.x += moveSpeed * dt;
+    position += direction.normalized() * moveSpeed * dt;
     super.update(dt);
   }
 }
